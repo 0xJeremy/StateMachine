6d7b58eac7c61dc7eaf597e595a5e6b571a43496
0xJeremy <jeremy.kanovsky@tufts.edu>
2020-01-12 13:20:57 -0500

    Updated machine run routine and state object

--- machine.py
+++ machine.py
@@ -8,6 +8,8 @@ class Machine():
 		self.finished = False
 		self.__initial = 
 		self.__valid = True
+		self.__uniqueHistory = []
+		self.__history = []
 
 	def setStates(self, states):
 		if self.states != {}:
@@ -27,7 +29,32 @@ class Machine():
 		self.__initial = initial
 
 	def start(self):
+		if self.started:
+			raise Exception("Machine cannot be started more than once")
+		self.stated = True
 		self.running = True
+		self.state = self.states[initial]
+		self.__run()
+
+	def __run(self):
+		while self.running:
+			self.state.__run(self)
+			self.__history.append(self.state)
+			if self.__uniqueHistory[-1] is not self.state.getName():
+				self.__uniqueHistory.append(self.state)
+			transition = self.state.__runTransition(self)
+			if transition is not None:
+				if transition is not in self.states.keys():
+					raise Exception("State {} returned invalid transition ({})".format(self.state.getName(), transition))
+				self.state = self.states[transition]
 
 	def pause(self):
+		if not self.started:
+			raise Exception("Machine cannot pause because it has not been started")
 		self.running = False
+
+	def unpause(self):
+		if not self.started:
+			raise Exception("Machine cannot unpause because it has not been started")
+		self.running = True
+		self.__run()
--- state.py
+++ state.py
@@ -1,33 +1,48 @@
+import inspect
+
 class State():
 	def __init__(self, name=None, exe=None, exeCapture=None, transition=None, transitionCapture=None):
-		self.name = name
-		self.exe = exe
-		self.transition = transition
-		self.valid = self.__valid()
+		self.setName(name)
+		self.setExe(exe)
+		self.setExeCapture(exeCapture)
+		self.setTransition(transition)
+		self.setTransitionCapture(transitionCapture)
+		self.__validate()
 
-	def __valid(self):
+	def __validate(self):
 		if self.name is not None and \
 		   self.exe is not None and \
 		   self.transition is not None:
-		   return True
-		return False
+		   self.valid = True
+		self.valid = False
 
 	def isValid(self):
+		self.__validate()
 		return self.valid
 
 	def getName(self):
 		return self.name
 
 	def setName(self, name):
+		if self.name is not None:
+			raise Exception("State name cannot be redefined once set")
 		self.name = name
+
 	def setExe(self, exe):
+		if not inspect.ismethod(exe) and not inspect.isfunction(exe):
+			raise Exception("Execution function is improperly formed")
 		self.exe = exe
+
 	def setExeCapture(self, exeCapture):
 		if self.exe is None:
 			raise Exception("The execution function must be defined first")
 		self.exeCapture = exeCapture
+
 	def setTransition(self, transition):
+		if not inspect.ismethod(exe) and not inspect.isfunction(exe):
+			raise Exception("Transition function is improperly formed")
 		self.transition = transition
+
 	def setTransitionCapture(self, transitionCapture):
 		if self.transition is None:
 			raise Exception("The transition function must be defined first")
